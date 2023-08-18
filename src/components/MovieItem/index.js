2fc16c14d361dca5be012c6588964bdf035c7d24
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-18 08:32:52 +0000

    funtionalities added, need to add styles

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,1 +1,39 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={
+          <button className="thumbnail-button" type="button">
+            <img src={thumbnailUrl} alt="thumbnail" />
+          </button>
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              data-testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose size="30" color="#616e7c" />
+            </button>
+            <ReactPlayer url={videoUrl} controls />
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
--- src/components/PrimeVideo/index.js
+++ src/components/PrimeVideo/index.js
@@ -1,1 +1,45 @@
-// Write your code here
+import {Component} from 'react'
+
+import MoviesSlider from '../MoviesSlider'
+
+import './index.css'
+
+class PrimeVideo extends Component {
+  //   state = {activeId: ''}
+
+  render() {
+    const {moviesList} = this.props
+    const actionMoviesList = moviesList.filter(
+      eachMovie => eachMovie.categoryId === 'ACTION',
+    )
+    const comedyMoviesList = moviesList.filter(
+      eachMovie => eachMovie.categoryId === 'COMEDY',
+    )
+    // console.log(actionMoviesList)
+    return (
+      <div className="app-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+          alt="prime video"
+          className="prime-image"
+        />
+        <div className="movies-list-container">
+          <div>
+            <h1 className="heading">Action Movies</h1>
+            <div className="slider-container">
+              <MoviesSlider moviesList={actionMoviesList} />
+            </div>
+          </div>
+          <div>
+            <h1 className="heading">Comedy Movies</h1>
+            <div className="slider-container">
+              <MoviesSlider moviesList={comedyMoviesList} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PrimeVideo
